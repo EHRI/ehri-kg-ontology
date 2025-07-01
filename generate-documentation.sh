2bda36fbe322bf5bbbba502f1c7bd0d3ff0f3140
Herminio García González <herminiogg@hotmail.com>
2025-07-01 14:14:37 +0200

    Added an initial set of documention and modified the ontology after an initial evaluation

---
+++ generate-documentation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -f widoco.jar ]; then
+    curl -L https://github.com/dgarijo/Widoco/releases/download/v1.4.25/widoco-1.4.25-jar-with-dependencies_JDK-11.jar -o widoco.jar
+fi
+
+java -jar widoco.jar -ontFile ./ontology/ehri.owl -outFolder ./documentation -getOntologyMetadata -oops -rewriteAll -htaccess -uniteSections -webVowl
