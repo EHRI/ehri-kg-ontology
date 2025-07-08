a88c12cbaed95f136cac4a984c1dacaef45f58fa
Herminio García González <herminiogg@hotmail.com>
2025-07-08 10:14:40 +0200

    Added some additional definitions and corrected some mistakes

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -4,4 +4,5 @@ if [ ! -f widoco.jar ]; then
     curl -L https://github.com/dgarijo/Widoco/releases/download/v1.4.25/widoco-1.4.25-jar-with-dependencies_JDK-11.jar -o widoco.jar
 fi
 
-java -jar widoco.jar -ontFile ./ontology/ehri.owl -outFolder ./documentation -getOntologyMetadata -oops -rewriteAll -htaccess -uniteSections -webVowl
+#java -jar widoco.jar -ontFile ./ontology/ehri.owl -outFolder ./documentation -getOntologyMetadata -oops -rewriteAll -htaccess -uniteSections -excludeProvenance
+java -jar widoco.jar -ontFile ./ontology/ehri.owl -outFolder ./documentation -getOntologyMetadata -oops -rewriteAll -htaccess -excludeProvenance
