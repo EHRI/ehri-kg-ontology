3f3bebba009dbabc884a015f6d004c0882fa43df
Herminio García González <herminiogg@hotmail.com>
2025-07-15 13:38:46 +0200

    Adapted the ontology generation script to add the custom resources.

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -4,5 +4,11 @@ if [ ! -f widoco.jar ]; then
     curl -L https://github.com/dgarijo/Widoco/releases/download/v1.4.25/widoco-1.4.25-jar-with-dependencies_JDK-11.jar -o widoco.jar
 fi
 
-#java -jar widoco.jar -ontFile ./ontology/ehri.owl -outFolder ./documentation -getOntologyMetadata -oops -rewriteAll -htaccess -uniteSections -excludeProvenance
 java -jar widoco.jar -ontFile ./ontology/ehri.owl -outFolder ./documentation -getOntologyMetadata -oops -rewriteAll -htaccess -excludeProvenance
+
+# Include static sections in the generated documentation
+cp documentationAdditionalContent/introduction-en.html documentation/sections/introduction-en.html
+cp documentationAdditionalContent/references-en.html documentation/sections/references-en.html
+mkdir documentation/images
+cp documentationAdditionalContent/ehriPortalDataModel.png documentation/images/ehriPortalDataModel.png
+cat documentationAdditionalContent/custom.css >> documentation/resources/extra.css
