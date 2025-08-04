50da134f8bf152813e6f8dacdbfc967775acfb0f
Herminio García González <herminiogg@hotmail.com>
2025-08-04 12:55:03 +0200

    Modified ontology according to the agreed changes, see #2

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -4,7 +4,10 @@ if [ ! -f widoco.jar ]; then
     curl -L https://github.com/dgarijo/Widoco/releases/download/v1.4.25/widoco-1.4.25-jar-with-dependencies_JDK-11.jar -o widoco.jar
 fi
 
+# Generate documentation with OOPS evaluation
 java -jar widoco.jar -ontFile ./ontology/ehri.owl -outFolder ./documentation -getOntologyMetadata -oops -rewriteAll -htaccess -excludeProvenance
+# Generate documentation without OOPS evaluation
+java -jar widoco.jar -ontFile ./ontology/ehri.owl -outFolder ./documentation -getOntologyMetadata -rewriteAll -htaccess -excludeProvenance
 
 # Include static sections in the generated documentation
 cp documentationAdditionalContent/introduction-en.html documentation/sections/introduction-en.html
@@ -12,3 +15,4 @@ cp documentationAdditionalContent/references-en.html documentation/sections/refe
 mkdir documentation/images
 cp documentationAdditionalContent/ehriPortalDataModel.png documentation/images/ehriPortalDataModel.png
 cat documentationAdditionalContent/custom.css >> documentation/resources/extra.css
+sed -i 's/<a href="https:\/\/www\.ica\.org\/standards\/RiC\/ontology">ontology<\/a>/<a href="https:\/\/www\.ica\.org\/standards\/RiC\/ontology\/1\.0\.2">RiC-O 1\.0\.2<\/a>/g' documentation/index-en.html
